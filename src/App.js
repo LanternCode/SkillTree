4ab03abe358f3b9256e26af335baf04f094a988d
Lantern <leanboxwebdev@gmail.com>
2019-05-12 16:31:40 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,183 @@
+import React, { Component } from "react";
+import Skill from "./components/Skill";
+import "./styles/App.css";
+
+class App extends Component {
+    state = {
+        skills: [
+            {
+                id: 0,
+                name: "Czytanie",
+                description: "aaaaaa",
+                unlocked: false,
+                depth: 1,
+                requirements: []
+            },
+            {
+                id: 1,
+                name: "Pisanie",
+                description: "aaaaaaa",
+                unlocked: false,
+                depth: 1,
+                requirements: []
+            },
+            {
+                id: 2,
+                name: "Gra",
+                description: "aaaaaa",
+                unlocked: false,
+                depth: 1,
+                requirements: []
+            },
+            {
+                id: 3,
+                name: "Pisanie",
+                description: "aaaaaaa",
+                unlocked: false,
+                depth: 1,
+                requirements: []
+            },
+            {
+                id: 4,
+                name: "Pisanie",
+                description: "aaaaaaa",
+                unlocked: false,
+                depth: 2,
+                requirements: [0]
+            },
+            {
+                id: 5,
+                name: "Pisanie",
+                description: "aaaaaaa",
+                unlocked: false,
+                depth: 2,
+                requirements: [0, 1]
+            },
+            {
+                id: 6,
+                name: "Pisanie",
+                description: "aaaaaaa",
+                unlocked: false,
+                depth: 2,
+                requirements: [1, 2]
+            },
+            {
+                id: 7,
+                name: "Pisanie",
+                description: "aaaaaaa",
+                unlocked: false,
+                depth: 2,
+                requirements: [2, 3]
+            },
+            {
+                id: 8,
+                name: "Pisanie",
+                description: "aaaaaaa",
+                unlocked: false,
+                depth: 2,
+                requirements: [3]
+            },
+            {
+                id: 9,
+                name: "Pisanie",
+                description: "aaaaaaa",
+                unlocked: false,
+                depth: 3,
+                requirements: [4, 5]
+            },
+            {
+                id: 10,
+                name: "Pisanie",
+                description: "aaaaaaa",
+                unlocked: false,
+                depth: 3,
+                requirements: [5, 6]
+            },
+            {
+                id: 11,
+                name: "Pisanie",
+                description: "aaaaaaa",
+                unlocked: false,
+                depth: 3,
+                requirements: [6, 7]
+            },
+            {
+                id: 12,
+                name: "Pisanie",
+                description: "aaaaaaa",
+                unlocked: false,
+                depth: 3,
+                requirements: [7, 8]
+            }
+        ],
+        maximumSkillPoints: 5,
+        currentSkillPoints: 0,
+        ownedSkills: [],
+        depthReached: 0
+    };
+
+    handleLock = (skill, cannotBeUnlocked) => {
+        const skills = [...this.state.skills];
+
+        if (skill.unlocked) {
+            const currentSkillPoints = this.state.currentSkillPoints - 1;
+            this.setState({ currentSkillPoints });
+            skill.unlocked = !skill.unlocked;
+            skills[skill.id] = skill;
+
+            let ownedSkills = [...this.state.ownedSkills];
+
+            ownedSkills.map((mapedSkill, i) => {
+                if (mapedSkill === skill.id) {
+                    ownedSkills[i] = null;
+                }
+                return null;
+            });
+            ownedSkills = ownedSkills.filter(id => {
+                if (id === 0) return "0";
+                else return id != null;
+            });
+            this.setState({ ownedSkills });
+
+            this.setState({ skills });
+        } else if (!cannotBeUnlocked) {
+            const currentSkillPoints = this.state.currentSkillPoints + 1;
+            if (currentSkillPoints <= this.state.maximumSkillPoints) {
+                this.setState({ currentSkillPoints });
+                skill.unlocked = !skill.unlocked;
+                skills[skill.id] = skill;
+
+                const ownedSkills = [skill.id, ...this.state.ownedSkills];
+                this.setState({ ownedSkills });
+
+                this.setState({ skills });
+            }
+        } else {
+            //skill cannot be unlocked as it requires preceeding skills
+        }
+    };
+
+    render() {
+        return (
+            <div>
+                Remaining Skill Points:{" "}
+                {this.state.maximumSkillPoints - this.state.currentSkillPoints}
+                <div className="skills">
+                    <br />
+                    {this.state.skills.map(skill => {
+                        return (
+                            <Skill
+                                key={skill.id}
+                                skill={skill}
+                                unlockedSkills={this.state.ownedSkills}
+                                handleLock={this.handleLock}
+                            />
+                        );
+                    })}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/components/Skill.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import "../styles/Skill.css";
+
+class Skill extends Component {
+    render() {
+        let className = "skill skill-depth-" + this.props.skill.depth;
+
+        let cannotBeUnlocked = this.props.skill.depth !== 1 ? true : false;
+        this.props.skill.requirements.map(requiredSkill => {
+            if (this.props.unlockedSkills.includes(requiredSkill))
+                cannotBeUnlocked = false;
+            return null;
+        });
+
+        className = cannotBeUnlocked
+            ? className + " skill-cannotBeUnlocked"
+            : this.props.skill.unlocked
+            ? className + " skill-unlocked"
+            : className;
+
+        return (
+            <div
+                className={className}
+                onClick={() => {
+                    this.props.handleLock(this.props.skill, cannotBeUnlocked);
+                }}
+            >
+                {cannotBeUnlocked ? "?" : this.props.skill.name}
+                <br />
+                {cannotBeUnlocked ? "" : this.props.skill.description}
+            </div>
+        );
+    }
+}
+
+export default Skill;
